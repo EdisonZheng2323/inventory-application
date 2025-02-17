310b1ef79ece0f913001dcce0e11950013e8607d
EdisonZheng2323 <edisonzheng3737@gmail.com>
2025-02-16 21:05:27 -0500

    Connect project to railway database

--- db/populatedb.js
+++ db/populatedb.js
@@ -16,7 +16,7 @@ CREATE TABLE toys (
 
 async function main(){
   const client = new Client({
-    connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST_NAME}:${process.env.PORT}/${process.env.DATABASE}`
+    connectionString: `${process.env.DATABASE_URL}`
   });
   await client.connect();
   await client.query(SQL);
